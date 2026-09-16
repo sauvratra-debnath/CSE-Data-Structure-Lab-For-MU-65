050b72728ada66a45b4ed1cf82305c53714c583c
sauvratra-debnath <sauvratradebnathpranta@gmail.com>
2026-09-16 11:06:37 +0600

    Add files via upload

---
+++ Lab 2/YourWork/Structure_2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Address {
+       string city;
+       int zipCode;
+};
+
+struct Student {
+    int id;
+    string name;
+    Address studentAddress;
+};
+
+int main() {
+    Student s1;
+    s1.id = 102;
+    s1.name = "Rahim";
+    s1.studentAddress.city = "Sylhet";
+    s1.studentAddress.zipCode = 3100;
+
+    cout <<s1.name << " lives in "<<s1.studentAddress.city <<endl;
+    return 0;
+}
